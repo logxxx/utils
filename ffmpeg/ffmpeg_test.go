e9a186b779a61e2ebf96e94111ab20f98ebd0d30
logxxx <pennylane@yeah.net>
2024-04-27 17:26:38 +0800

    feat: gene smaller preview video

--- ffmpeg/ffmpeg_test.go
+++ ffmpeg/ffmpeg_test.go
@@ -1,10 +1,16 @@
 package ffmpeg
 
-import "testing"
+import (
+	log "github.com/sirupsen/logrus"
+	"testing"
+)
 
 func TestGenePreviewVideo(t *testing.T) {
-	filePath := "H:\\output_xhs\\20240316\\测试视频\\Gandy___这个背部错误，浪费你很多时间.mp4"
-	err := GenePreviewVideo(filePath, filePath+".thumb.mp4")
+
+	log.SetLevel(log.DebugLevel)
+
+	filePath := "H:\\output_xhs\\20240316\\测试竖屏\\1.mp4"
+	err := GenePreviewVideo(filePath, filePath+".111.thumb.mp4")
 	if err != nil {
 		panic(err)
 	}
--- ffmpeg/preview.go
+++ ffmpeg/preview.go
@@ -6,8 +6,21 @@ import (
 )
 
 func GenePreviewVideo(filePath string, toPath string) error {
-	command := "ffmpeg -y -i %v -to 15 -vf scale=640:-2 -pix_fmt yuv420p -level 4.2 -crf 21 -threads 4 -strict -2 %v"
-	command = fmt.Sprintf(command, filePath, toPath)
+
+	scale := "480:-1"
+	fpb := FFProbe("ffprobe")
+	vInfo, err := fpb.NewVideoFile(filePath)
+	if err == nil {
+		log.Infof("height:%v width:%v", vInfo.Height, vInfo.Width)
+		if vInfo.Height > vInfo.Width { //竖屏
+			scale = "-1:480"
+		}
+	} else {
+		log.Errorf("GenePreviewVideo NewVideoFile err:%v", err)
+	}
+
+	command := "ffmpeg -y -i %v -to 15 -vf scale=%v -pix_fmt yuv420p -level 4.2 -crf 30 -threads 8 -strict -2 %v"
+	command = fmt.Sprintf(command, filePath, scale, toPath)
 	output, err := runCommand(command)
 	log.Debugf("GenePreviewVideo command:%v output:%v err:%v", command, string(output), err)
 	if err != nil {
