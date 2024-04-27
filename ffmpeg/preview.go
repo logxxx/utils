5a09f43ec823a9e6d3109d90401ef0f7a51c31ac
logxxx <pennylane@yeah.net>
2024-04-27 17:01:55 +0800

    feat: gene preview video

---
+++ ffmpeg/preview.go
@@ -0,0 +1,17 @@
+package ffmpeg
+
+import (
+	"fmt"
+	"github.com/logxxx/utils/log"
+)
+
+func GenePreviewVideo(filePath string, toPath string) error {
+	command := "ffmpeg -y -i %v -to 15 -vf scale=640:-2 -pix_fmt yuv420p -level 4.2 -crf 21 -threads 4 -strict -2 %v"
+	command = fmt.Sprintf(command, filePath, toPath)
+	output, err := runCommand(command)
+	log.Debugf("GenePreviewVideo command:%v output:%v err:%v", command, string(output), err)
+	if err != nil {
+		return err
+	}
+	return nil
+}
